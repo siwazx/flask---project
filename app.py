b3234cd6a70c317accc33f04ca43e3eb96b24a48
MarTin <siwkons@gmail.com>
2026-02-25 00:23:28 +0700

    update menu

--- app.py
+++ app.py
@@ -10,11 +10,41 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
-
+    
 @app.route("/")
 def home():
     menus = Menu.query.all()
     return render_template("index.html", menus=menus)
+
+@app.route("/add")
+def add_menu():
+
+    menus = [
+        # ข้าวผัด
+        Menu(name="ข้าวผัดหมู", price=50),
+        Menu(name="ข้าวผัดไก่", price=50),
+        Menu(name="ข้าวผัดทะเล", price=60),
+        Menu(name="ข้าวผัดหมูกรอบ", price=60),
+
+        # กระเพรา
+        Menu(name="กระเพราหมู", price=50),
+        Menu(name="กระเพราไก่", price=50),
+        Menu(name="กระเพราทะเล", price=60),
+        Menu(name="กระเพราหมูกรอบ", price=60),
+
+        # เครื่องแกง
+        Menu(name="เครื่องแกงหมู", price=55),
+        Menu(name="เครื่องแกงไก่", price=55),
+        Menu(name="เครื่องแกงทะเล", price=65),
+        Menu(name="เครื่องแกงหมูกรอบ", price=65),
+    ]
+
+    for menu in menus:
+        db.session.add(menu)
+
+    db.session.commit()
+
+    return "เพิ่มเมนูเรียบร้อยแล้ว"
 
 if __name__ == "__main__":
     app.run(debug=True)
